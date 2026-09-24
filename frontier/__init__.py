2a8d8fd58f35a5848d7081c64db133fe5ddf3b3e
Loofy <ebh.ceo213@gmail.com>
2026-09-24 18:10:03 +0100

    Add reusable frontier engine

---
+++ frontier/__init__.py
@@ -0,0 +1,3 @@
+"""Reusable frontier implementation."""
+from .core import digest_event, reduce_events, render_projection, semantic_digest, semantic_projection, verify_repo
+__all__=["digest_event","reduce_events","render_projection","semantic_digest","semantic_projection","verify_repo"]
